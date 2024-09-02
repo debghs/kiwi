360781c2bbb997e27e70db0464f3abcf7a117350
debghs <dghosh31428@gmail.com>
2024-09-02 23:07:24 +0530

    minor fix

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -15,7 +15,7 @@ function Home() {
       <HeroSection />
       <Filter />
       <ProductCard />
-      <div className="flex justify-center -mt-10 mb-4">
+      <div className="flex justify-center -mt-1 mb-4">
         <Link to={'/allproducts'}>
           <button className=' bg-gray-300 px-5 py-2 rounded-xl'>See more</button>
         </Link>
