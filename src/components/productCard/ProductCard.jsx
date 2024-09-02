774024b4c6a2c520a873a748aaade6b60c9393e7
debghs <dghosh31428@gmail.com>
2024-09-02 23:17:08 +0530

    cosmetics

--- src/components/productCard/ProductCard.jsx
+++ src/components/productCard/ProductCard.jsx
@@ -27,7 +27,7 @@ function ProductCard() {
             <div className="container px-5 py-8 md:py-16 mx-auto">
                 <div className="lg:w-1/2 w-full mb-6 lg:mb-10">
                     <h1 className="sm:text-3xl text-2xl font-medium title-font mb-2 text-gray-900" style={{ color: mode === 'dark' ? 'white' : '' }}>Our Latest Collection</h1>
-                    <div className="h-1 w-20 bg-pink-600 rounded"></div>
+                    <div className="h-1 w-200 bg-pink-600 rounded"></div>
                 </div>
 
                 <div className="flex flex-wrap -m-4">
